671a1a131f9a9d4e3472ba19b67e9eed9d8ff935
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-06-02 23:48:18 -0600

    Renombrar tablas y columnas en la migración de roles y permisos

--- prisma/migrations/20260601035521_add_roles_permision/migration.sql
+++ prisma/migrations/20260601035521_add_roles_permision/migration.sql
@@ -2,6 +2,17 @@
 ALTER TABLE "mnt_users" ADD COLUMN     "role_id" INTEGER;
 
 -- CreateTable
+CREATE TABLE "mnt_permissions" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "mnt_permissions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "mnt_roles" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -13,17 +24,6 @@ CREATE TABLE "mnt_roles" (
 );
 
 -- CreateTable
-CREATE TABLE "mnt_group_roles" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3),
-    "deleted_at" TIMESTAMP(3),
-
-    CONSTRAINT "mnt_group_roles_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "mnt_user_roles" (
     "user_id" INTEGER NOT NULL,
     "permission_id" INTEGER NOT NULL,
@@ -32,24 +32,24 @@ CREATE TABLE "mnt_user_roles" (
 );
 
 -- CreateTable
-CREATE TABLE "mnt_group_roles_roles" (
-    "group_id" INTEGER NOT NULL,
+CREATE TABLE "mnt_role_permissions" (
+    "role_id" INTEGER NOT NULL,
     "permission_id" INTEGER NOT NULL,
 
-    CONSTRAINT "mnt_group_roles_roles_pkey" PRIMARY KEY ("group_id","permission_id")
+    CONSTRAINT "mnt_role_permissions_pkey" PRIMARY KEY ("role_id","permission_id")
 );
 
 -- AddForeignKey
-ALTER TABLE "mnt_users" ADD CONSTRAINT "mnt_users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "mnt_group_roles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "mnt_users" ADD CONSTRAINT "mnt_users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "mnt_roles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "mnt_user_roles" ADD CONSTRAINT "mnt_user_roles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "mnt_users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "mnt_user_roles" ADD CONSTRAINT "mnt_user_roles_permission_id_fkey" FOREIGN KEY ("permission_id") REFERENCES "mnt_roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "mnt_user_roles" ADD CONSTRAINT "mnt_user_roles_permission_id_fkey" FOREIGN KEY ("permission_id") REFERENCES "mnt_permissions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "mnt_group_roles_roles" ADD CONSTRAINT "mnt_group_roles_roles_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "mnt_group_roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "mnt_role_permissions" ADD CONSTRAINT "mnt_role_permissions_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "mnt_roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "mnt_group_roles_roles" ADD CONSTRAINT "mnt_group_roles_roles_permission_id_fkey" FOREIGN KEY ("permission_id") REFERENCES "mnt_roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "mnt_role_permissions" ADD CONSTRAINT "mnt_role_permissions_permission_id_fkey" FOREIGN KEY ("permission_id") REFERENCES "mnt_permissions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
