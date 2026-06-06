4edfe705d94822bb29ad6beeb0b54d478c193bc7
Carlos Escobar <es21001@ues.edu.sv>
2026-06-06 11:01:20 -0600

    feat: gestión de cotizaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CargoModule } from './cargo/cargo.module';
 import { EmpleadosModule } from './empleados/empleados.module';
+import { CotizacionModule } from './cotizaciones/cotizaciones.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, CargoModule, EmpleadosModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    CargoModule,
+    EmpleadosModule,
+    CotizacionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
