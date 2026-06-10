209426626eb7657b3b76b993d176b035835dd405
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-06-09 23:03:24 -0600

    refactor: mejorar formato de importaciones y comentarios en empleado.controller

--- src/empleados/controllers/empleado.controller.ts
+++ src/empleados/controllers/empleado.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Delete, Get, Param, Patch, ParseIntPipe, Post } from '@nestjs/common';
-import { EmpleadoService } from '../service/empleado.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateEmpleadoDto } from '../dto/createEmpleado';
 import { UpdateEmpleadoDto } from '../dto/updateEmpleado';
+import { EmpleadoService } from '../service/empleado.service';
 
-//estos son los endpoint 
+//estos son los endpoint
 // Controlador REST para `empleados`. Rutas sencillas y comentarios en español.
 @Controller('empleados')
 export class EmpleadoController {
@@ -42,13 +51,15 @@ export class EmpleadoController {
     return this.empleadoService.remove(id);
   }
 
-// GPRCIMPER-90: Guardar habilidades desde el perfil del empleado
-  @Patch(':id/habilidades')
-  async updateHabilidades(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() body: { habilidadesIds: number[] },
-  ) {
-    return await this.empleadoService.updateHabilidades(id, body.habilidadesIds);
-  }
-
+  // // GPRCIMPER-90: Guardar habilidades desde el perfil del empleado
+  // @Patch(':id/habilidades')
+  // async updateHabilidades(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Body() body: { habilidadesIds: number[] },
+  // ) {
+  //   return await this.empleadoService.updateHabilidades(
+  //     id,
+  //     body.habilidadesIds,
+  //   );
+  // }
 }
