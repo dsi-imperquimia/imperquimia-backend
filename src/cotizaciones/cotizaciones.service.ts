c029c984af35c6697ae5b48bbc1be0ed198be531
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-06-09 19:20:59 -0600

    fix: corregir asignación de campos de teléfono y correo en la creación de cotizaciones

--- src/cotizaciones/cotizaciones.service.ts
+++ src/cotizaciones/cotizaciones.service.ts
@@ -1,6 +1,6 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable, BadRequestException } from '@nestjs/common';
 import { Cotizacion, Prisma } from '@gen/prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCotizacionDto } from './dto/create-cotizacion.dto';
 import { UpdateCotizacionDto } from './dto/update-cotizacion.dto';
 
@@ -221,8 +221,8 @@ export class CotizacionService {
         data: {
           descripcion: data.descripcion,
           cliente: data.cliente,
-          phone: data.cliente,
-          email: data.phone,
+          phone: data.phone,
+          email: data.email,
           estado: data.estado,
           subTotal: subTotalCotizacion,
           totalIva: totalIvaCotizacion,
